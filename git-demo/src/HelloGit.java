a98e176f6db4cf6e7e5c69f489654b5e9149645c
onwaylee <358033145@qq.com>
2020-01-01 13:58:13 +0800

    提交测试

---
+++ git-demo/src/HelloGit.java
@@ -0,0 +1,17 @@
+/**
+ * @author udady
+ *LastUpdate 2020年1月1日 下午1:41:26
+ */
+/**
+* @author 作者
+* @version 创建时间：2020年1月1日 下午1:41:26
+* 类说明
+*/
+public class HelloGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloGit");
+	}
+
+}
