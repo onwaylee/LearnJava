399162c5330b87cba26f52f9321e80e6925ac1bd
ursowonderful <ursowonderful@DESKTOP-NT2NVC5>
2020-01-01 15:04:28 +0800

    change in HelloGit
    Signed-off-by: ursowonderful <ursowonderful@DESKTOP-NT2NVC5>

--- git-demo/src/HelloGit.java
+++ git-demo/src/HelloGit.java
@@ -1,18 +1,18 @@
 /**
  * @author udady
- *LastUpdate 2020年1月1日 下午1:41:26
+ *LastUpdate 2020骞�1鏈�1鏃� 涓嬪崍1:41:26
  */
 /**
-* @author 作者
-* @version 创建时间：2020年1月1日 下午1:41:26
-* 类说明
+* @author 浣滆��
+* @version 鍒涘缓鏃堕棿锛�2020骞�1鏈�1鏃� 涓嬪崍1:41:26
+* 绫昏鏄�
 */
 public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HelloGit");
-		System.out.println("HelloGit111");
+		System.out.println("HelloGit again");
 	}
 
 }
